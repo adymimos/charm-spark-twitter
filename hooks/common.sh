a6e32870ce115663971fb4f1f27baf14e669a18b
Samuel Cozannet <samnco@gmail.com>
2015-05-19 11:03:51 +0200

    merge with no compile version and adapt actions

--- hooks/common.sh
+++ hooks/common.sh
@@ -3,6 +3,9 @@
 # Global Variables for the charm
 APPNAME="rts.spark"
 DEST_FOLDER="/opt/spark"
-CONFIG_FILE="${DEST_FOLDER}/${APPNAME}/src/main/resources/rts.spark.properties"
+#CONFIG_FILE="${DEST_FOLDER}/${APPNAME}/src/main/resources/rts.spark.properties"
+CONFIG_FILE_NAME="rts.spark.properties"
+CONFIG_FILE="/home/ubuntu/$CONFIG_FILE_NAME"
 NODEJS_APP_URI="post"
-JARFILE="target/rts.spark-0.0.1.jar"
+#JARFILE="target/rts.spark-0.0.1.jar"
+JARFILE="rts.spark-0.0.1.jar"
